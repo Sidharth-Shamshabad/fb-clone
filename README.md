869306f8f63a6baba27f3c305b77faa11996cf33
Sidharth Shamshabad <sid@techvoe.com>
2020-10-06 03:07:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fb-clone
+ Facebook website clone using React JS and Firebase! Tutorial here: https://www.youtube.com/watch?v=B-kxUMHBxNo
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCf992_VcgWqrkCUWhrHMTaRwt9BlLeU6I",
+    authDomain: "facebook-clone-b8b98.firebaseapp.com",
+    databaseURL: "https://facebook-clone-b8b98.firebaseio.com",
+    projectId: "facebook-clone-b8b98",
+    storageBucket: "facebook-clone-b8b98.appspot.com",
+    messagingSenderId: "61508404732",
+    appId: "1:61508404732:web:e30f1c5e7794e985649e79",
+    measurementId: "G-PLPB11N9W1"
+  };
